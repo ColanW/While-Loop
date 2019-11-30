d161cc5973e02e3645bd62105b870d41fa7f3273
ColanW <54151642+ColanW@users.noreply.github.com>
2019-11-30 04:10:20 -0600

    Add files via upload

---
+++ WhileLoop/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+		while(true) {
+			
+			System.out.println("True");
+			
+		}
+		System.out.println();
+	}
+	
+}
